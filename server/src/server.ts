b2fe79d0e941f4fec27baa8558ae1a7f03709cd7
josh-hensley <me@joshhensley.com>
2024-12-22 17:37:43 -0600

    fixed production bug, serving build folder that doesn't exist

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,12 @@ import path from 'node:path';
 import db from './config/connection.js';
 import routes from './routes/index.js';
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -11,7 +17,7 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
 }
 
 app.use(routes);
