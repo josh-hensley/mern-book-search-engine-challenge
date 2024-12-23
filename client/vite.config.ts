061c291229b46cf051e0843d6fab83d7463fdebb
josh-hensley <me@joshhensley.com>
2024-12-23 14:57:25 -0600

    added dev dependency for client side port access

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const PORT = process.env.PORT;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -9,7 +11,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/graphql': {
-        target: 'http://localhost:3001',
+        target: `http://localhost:${PORT}`,
         secure: false,
         changeOrigin: true
       }
